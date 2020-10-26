18e54914af8fde85d9954d86eae15b73a30d99fe
[Alex <atwilliams88@gmail.com>
2020-10-25 23:29:39 -0500

    Fixed bug in menu animation

--- static/scripts/src/home_page_scripts/togglenav.js
+++ static/scripts/src/home_page_scripts/togglenav.js
@@ -2,12 +2,16 @@ export const toggleNav = () => {
   const nav = document.querySelector("#hamburgerIcon");
   const closeSidebar = document.querySelector("#closeSidebar");
   const sidebar = document.querySelector("#sidebar");
+  const main = document.querySelector("main");
   if (nav) {
     nav.addEventListener("click", () => {
       const isSidebarHidden = Array.from(sidebar.classList).indexOf("hidden");
       if (isSidebarHidden > -1) {
         sidebar.classList.remove("hidden");
         sidebar.classList.add("slide-right");
+        setTimeout(() => {
+          main.classList.add("hidden");
+        }, 300);
       }
     });
   }
@@ -15,6 +19,7 @@ export const toggleNav = () => {
     closeSidebar.addEventListener("click", () => {
       sidebar.classList.remove("slide-right");
       sidebar.classList.add("slide-left");
+      main.classList.remove("hidden");
       setTimeout(() => {
         sidebar.classList.add("hidden");
         sidebar.classList.remove("slide-left");
