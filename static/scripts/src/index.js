b308393c1b32c0cb7900ade636d4936e98154699
[Alex <atwilliams88@gmail.com>
2020-10-26 23:15:13 -0500

    test name change of github

--- static/scripts/src/index.js
+++ static/scripts/src/index.js
@@ -2,6 +2,6 @@ import css from "../../styles/main.css";
 import { toggleNav } from "./home_page_scripts/togglenav";
 
 window.addEventListener("DOMContentLoaded", (e) => {
-  console.log("DOM fully loaded and parsed");
+  console.log("DOM fully loaded and parsed2");
   toggleNav();
 });
