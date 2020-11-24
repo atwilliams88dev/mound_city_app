ebeeda8c00c54a0a9c710a43fc53f32f967cad94
[Alex <atwilliams88@gmail.com>
2020-11-23 20:27:05 -0600

    added Blog details and dummy first blog post

--- routes/blog.js
+++ routes/blog.js
@@ -8,6 +8,59 @@ const blog_router = new Router();
 // export our router to be mounted by the parent application
 
 blog_router.get("/blog", async (req, res) => {
-  res.render("blog.njk");
+    //   const { id } = req.params;
+    const { rows } = await db.query("SELECT * FROM posts WHERE active = true");
+    console.log(rows)
+  // db.query('SELECT * from "mound_users" ', (err, result) => {
+  //   if (err) {
+  //     console.error(err + "error");
+  //   }
+  //   console.log(JSON.stringify(result.rows[0].email));
+  //   const email = result.rows[0].email;
+  //   res.render("home.njk", { email: email });
+  // });
+  // try {
+  //   const { rows } = await db.query('SELECT * FROM "mound_users" WHERE id=$1', [
+  //     1,
+  //   ]);
+  //   const { email } = rows[0];
+  //   res.render("home.njk", { email: email });
+  // } catch (err) {
+  //   logger.info(`in combo`);
+
+  //   logger.error(`${err}`);
+
+  //   res.status(500).render("500.njk");
+  // }
+  res.render("blog.njk", {rows:rows});
+});
+
+
+blog_router.get("/blog/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await db.query("SELECT * FROM posts WHERE id = $1", [id]);
+  console.log(rows)
+// db.query('SELECT * from "mound_users" ', (err, result) => {
+//   if (err) {
+//     console.error(err + "error");
+//   }
+//   console.log(JSON.stringify(result.rows[0].email));
+//   const email = result.rows[0].email;
+//   res.render("home.njk", { email: email });
+// });
+// try {
+//   const { rows } = await db.query('SELECT * FROM "mound_users" WHERE id=$1', [
+//     1,
+//   ]);
+//   const { email } = rows[0];
+//   res.render("home.njk", { email: email });
+// } catch (err) {
+//   logger.info(`in combo`);
+
+//   logger.error(`${err}`);
+
+//   res.status(500).render("500.njk");
+// }
+  res.render("blog_detail.njk", {article:rows[0]});
 });
 module.exports = blog_router;
