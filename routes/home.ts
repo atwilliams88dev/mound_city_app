9e5b7dd2005d1e27aa604a63590d6b5fdad69792
[Alex <atwilliams88@gmail.com>
2021-01-10 13:39:18 -0600

    added prettier for njk and switched to typescript

--- routes/home.ts
+++ routes/home.ts
@@ -1,14 +1,24 @@
+import { Request, Response } from "express";
 const Router = require("express-promise-router");
 const db = require("../db");
 const logger = require("../logger");
-const homeContent = require("../content/home");
+import {
+  ELEVATOR_PITCH,
+  ELEVATOR_PITCH_EXPANDED,
+  APPLICATION_DEVELOPMENT,
+  APP_DESIGN,
+  CONTENT_CREATION,
+  BRAIN_STORMING,
+  ANIMATION,
+  VIDEOGRAPHY,
+} from "../content/home";
 // create a new express-promise-router
 // this has the same API as the normal express router except
 // it allows you to use async functions as route handlers
 const home_router = new Router();
 // export our router to be mounted by the parent application
 
-home_router.get("/", async (req, res) => {
+home_router.get("/", async (req: Request, res: Response) => {
   //   const { id } = req.params;
   //   const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [id]);
   // db.query('SELECT * from "mound_users" ', (err, result) => {
@@ -32,16 +42,28 @@ home_router.get("/", async (req, res) => {
 
   //   res.status(500).render("500.njk");
   // }
-  res.render("home.njk", {
-    phoneNumber: homeContent.PHONE_NUMBER,
-    elevatorPitch: homeContent.ELEVATOR_PITCH,
-    elevatorPitchExpanded: homeContent.ELEVATOR_PITCH_EXPANDED,
-    applicationDevelopment: homeContent.APPLICATION_DEVELOPMENT,
-    contentCreation: homeContent.CONTENT_CREATION,
-    brainStorming: homeContent.BRAIN_STORMING,
-    appDesign: homeContent.APP_DESIGN,
-    animation: homeContent.ANIMATION,
-    videography: homeContent.VIDEOGRAPHY,
-  });
+  type HomeContextType = {
+    phoneNumber: string;
+    elevatorPitch: string;
+    elevatorPitchExpanded: string;
+    applicationDevelopment: string;
+    contentCreation: string;
+    brainStorming: string;
+    appDesign: string;
+    animation: string;
+    videography: string;
+  };
+  const HomeContext: HomeContextType = {
+    phoneNumber: process.env.PHONE_NUMBER,
+    elevatorPitch: ELEVATOR_PITCH,
+    elevatorPitchExpanded: ELEVATOR_PITCH_EXPANDED,
+    applicationDevelopment: APPLICATION_DEVELOPMENT,
+    contentCreation: CONTENT_CREATION,
+    brainStorming: BRAIN_STORMING,
+    appDesign: APP_DESIGN,
+    animation: ANIMATION,
+    videography: VIDEOGRAPHY,
+  };
+  res.render("home.njk", HomeContext);
 });
 module.exports = home_router;
