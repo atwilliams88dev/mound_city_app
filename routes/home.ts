85fab08d7fe25f89d85dac744b03540a1cbc15b6
[Alex <atwilliams88@gmail.com>
2021-04-13 20:45:02 -0500

    Minor - fix typo in email address

--- routes/home.ts
+++ routes/home.ts
@@ -19,29 +19,6 @@ const home_router = Router();
 // export our router to be mounted by the parent application
 
 home_router.get("/", async (req: Request, res: Response) => {
-  //   const { id } = req.params;
-  //   const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [id]);
-  // db.query('SELECT * from "mound_users" ', (err, result) => {
-  //   if (err) {
-  //     console.error(err + "error");
-  //   }
-  //   console.log(JSON.stringify(result.rows[0].email));
-  //   const email = result.rows[0].email;
-  //   res.render("home.njk", { email: email });
-  // });
-  // try {
-  //   const { rows } = await db.query('SELECT * FROM "mound_users" WHERE id=$1', [
-  //     1,
-  //   ]);
-  //   const { email } = rows[0];
-  //   res.render("home.njk", { email: email });
-  // } catch (err) {
-  //   logger.info(`in combo`);
-
-  //   logger.error(`${err}`);
-
-  //   res.status(500).render("500.njk");
-  // }
   type HomeContextType = {
     phoneNumber: string;
     elevatorPitch: string;
